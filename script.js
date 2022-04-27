c5f8bc6c976ef468419a41687392b903c507ada1
BrunoBernardini <bruno.f.bernardini@gmail.com>
2022-04-27 22:01:59 +0200

    Stampa di ogni card dei membri del team.

--- script.js
+++ script.js
@@ -6,3 +6,50 @@ BONUS:
 Utilizzare gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team: cliccando sul pulsante “add” viene creato un **nuovo oggetto**, il quale viene **inserito nell’array iniziale** e viene stampata una nuova card con tutte le informazioni inserite dall’utente.
  */
 
+const teamContainer = document.querySelector(".team-container");
+
+const teamCardList = [
+  {
+    nome: "Wayne Barnett",
+    ruolo: "Founder & CEO",
+    foto: "img/wayne-barnett-founder-ceo.jpg"
+  },
+  {
+    nome: "Angela Caroll",
+    ruolo: "Chief Editor",
+    foto: "img/angela-caroll-chief-editor.jpg"
+  },
+  {
+    nome: "Walter Gordon",
+    ruolo: "Office Manager",
+    foto: "img/walter-gordon-office-manager.jpg"
+  },
+  {
+    nome: "Angela Lopez",
+    ruolo: "Social Media Manager",
+    foto: "img/angela-lopez-social-media-manager.jpg"
+  },
+  {
+    nome: "Scott Estrada",
+    ruolo: "Developer",
+    foto: "img/scott-estrada-developer.jpg"
+  },
+  {
+    nome: "Barbara Ramos",
+    ruolo: "Graphic Designer",
+    foto: "img/barbara-ramos-graphic-designer.jpg"
+  }
+];
+
+for(let i=0; i<teamCardList.length; i++){
+  const teamCard = teamCardList[i];
+  teamContainer.innerHTML += `<div class="team-card">
+                                <div class="card-image">
+                                  <img src="${teamCard.foto}" alt="${teamCard.nome}"/>
+                                </div>
+                                <div class="card-text">
+                                  <h3>${teamCard.nome}</h3>
+                                  <p>${teamCard.ruolo}</p>
+                                </div>
+                              </div>`;
+}
